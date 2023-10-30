a96c1ad712c628fbee306dd62be0e12e97a00cb9
NandkishorC <chechar.nandkishor@gmail.com>
2023-10-30 21:53:53 +0530

    Commit code

---
+++ folderstructure/folderstructure/dev/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.78.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "TerraformRG"
+    storage_account_name = "kishorstr"
+    container_name       = "container1"
+    key                  = "dev.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+}
